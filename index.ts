724079753701eb3839332c0199fa9a7e03e89532
TTRider <software@ttrider.com>
2020-03-30 12:37:20 -0700

    generator

---
+++ index.ts
@@ -0,0 +1,102 @@
+import fs from "fs";
+import path from "path";
+import { promisify } from "util";
+const parse = require('csv-parse/lib/sync');
+
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+const readDir = promisify(fs.readdir);
+
+exec().then(() => console.info("done"));
+
+async function exec() {
+
+    const dayData: any[][] = [];
+
+    console.info("reading data");
+
+    const dir = path.resolve("data", "csse_covid_19_data", "csse_covid_19_daily_reports");
+
+    const files = await readDir(dir);
+
+    const dataSet = files
+        .filter(item => item.endsWith(".csv"))
+        .map(item => {
+
+            const dts = new Date(item.substr(0, item.length - 4)).toDateString();
+
+            return {
+                date: dts.substring(0, dts.length - 5),
+                path: path.resolve(dir, item)
+            }
+
+        });
+
+    for (const item of dataSet) {
+
+        const dayItem = (await readFile(item.path)).toString();
+
+        const d = parse(dayItem) as string[][];
+
+        //6, 8, 
+        // check mode
+        if (d[0].length === 6 || d[0].length === 8) {
+
+            const lines = d
+                .filter((line, index) => {
+                    if (line[1] != "US") return undefined;
+                    if (line[0] === "Washington") return line;
+                    if (line[0].endsWith(", WA")) return line;
+
+                    return undefined;
+                });
+
+            const dataPoints = lines.reduce((dp, item) => {
+                dp[1] += (item[3] ? parseInt(item[3]) : 0);
+                dp[2] += (item[4] ? parseInt(item[4]) : 0);
+                dp[3] += (item[5] ? parseInt(item[5]) : 0);
+                return dp;
+            }, [item.date, 0, 0, 0] as any[]);
+
+            dayData.push(dataPoints);
+
+        } else {
+
+            const lines = d
+                .filter((line, index) => {
+                    if (line[3] != "US") return undefined;
+                    if (line[2] === "Washington") return line;
+                    return undefined;
+                });
+
+
+            const dataPoints = lines.reduce((dp, item) => {
+                dp[1] += parseInt(item[7]);
+                dp[2] += parseInt(item[8]);
+                dp[3] += parseInt(item[9]);
+                return dp;
+            }, [item.date, 0, 0, 0] as any[]);
+
+            dayData.push(dataPoints);
+        }
+
+
+
+    }
+
+    const values = dayData.reduce((vals, item) => {
+
+        if (item[1] > 1) {
+            vals.labels.push(item[0]);
+            vals.confirmed.push(item[1]);
+            vals.deaths.push(item[2]);
+            vals.recovered.push(item[3]);
+        }
+        return vals;
+    }, { labels: [], confirmed: [], recovered: [], deaths: [] } as { labels: string[], confirmed: number[], recovered: number[], deaths: number[] });
+
+    await writeFile("./data.js", "data = " + JSON.stringify(values, null, 4));
+}
+
+
---
+++ run.sh
@@ -0,0 +1,9 @@
+git pull
+git pull --recurse-submodules
+npm i
+tsc ./index.ts
+node index
+open http://localhost:9000
+python -m SimpleHTTPServer 9000
+
+
