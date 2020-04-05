37b85dbd6603d1bc6d39168d1ec06ece2118af0f
TTRider <software@ttrider.com>
2020-04-05 15:27:46 -0700

    switching to state vars

--- index.ts
+++ index.ts
@@ -12,27 +12,22 @@ exec().then(() => console.info("done"));
 
 async function exec() {
 
+    const dataSet = await readData();
+    const waDayData = await getWAData(dataSet);
+    const waValues = await compute(waDayData);
+
+    const nyDayData = await getNYData(dataSet);
+    const nyValues = await compute(nyDayData);
+
+    await writeFile("./data.js", "var wa_data = " + JSON.stringify(waValues, null, 4)
+    + "\nvar ny_data = " + JSON.stringify(nyValues, null, 4));
+}
+
+async function getWAData(dataSet: {
+    date: string;
+    path: string;
+}[]) {
     const dayData: any[][] = [];
-
-    console.info("reading data");
-
-    const dir = path.resolve("data", "csse_covid_19_data", "csse_covid_19_daily_reports");
-
-    const files = await readDir(dir);
-
-    const dataSet = files
-        .filter(item => item.endsWith(".csv"))
-        .map(item => {
-
-            const dts = new Date(item.substr(0, item.length - 4)).toDateString();
-
-            return {
-                date: dts.substring(0, dts.length - 5),
-                path: path.resolve(dir, item)
-            }
-
-        });
-
     for (const item of dataSet) {
 
         const dayItem = (await readFile(item.path)).toString();
@@ -80,10 +75,105 @@ async function exec() {
 
             dayData.push(dataPoints);
         }
-
-
-
     }
+
+    return dayData;
+}
+
+async function getNYData(dataSet: {
+    date: string;
+    path: string;
+}[]) {
+
+
+    const dayData: any[][] = [];
+    for (const item of dataSet) {
+
+        const dayItem = (await readFile(item.path)).toString();
+
+        const d = parse(dayItem) as string[][];
+
+        //6, 8, 
+        // check mode
+        if (d[0].length === 6 || d[0].length === 8) {
+
+            const lines = d
+                .filter((line, index) => {
+                    if (line[1] != "US") return undefined;
+                    if (line[0] === "New Jersey") return line;
+                    if (line[0] === "New York") return line;
+                    return undefined;
+                });
+
+            const dataPoints = lines.reduce((dp, item) => {
+                dp[1] += (item[3] ? parseInt(item[3]) : 0);
+                dp[2] += (item[4] ? parseInt(item[4]) : 0);
+                dp[3] += (item[5] ? parseInt(item[5]) : 0);
+                return dp;
+            }, [item.date, 0, 0, 0] as any[]);
+
+            dayData.push(dataPoints);
+
+        } else {
+
+            const lines = d
+                .filter((line, index) => {
+                    if (line[3] != "US") return undefined;
+
+                    if (line[2] === "New York") {
+                        if (line[1] === "Westchester") return line;
+                        if (line[1] === "Bronx") return line;
+                        if (line[1] === "New York City") return line;
+                        if (line[1] === "Kings") return line;
+                        if (line[1] === "Richmond") return line;
+                        if (line[1] === "Queens") return line;
+                    };
+                    if (line[2] === "New Jersey") {
+                        if (line[1] === "Bergen") return line;
+                        if (line[1] === "Hudson") return line;
+                        if (line[1] === "Passaic") return line;
+                        if (line[1] === "Essex") return line;
+                        if (line[1] === "Union") return line;
+                    };
+                    return undefined;
+                });
+
+
+            const dataPoints = lines.reduce((dp, item) => {
+                dp[1] += parseInt(item[7]);
+                dp[2] += parseInt(item[8]);
+                dp[3] += parseInt(item[9]);
+                return dp;
+            }, [item.date, 0, 0, 0] as any[]);
+
+            dayData.push(dataPoints);
+        }
+    }
+
+    return dayData;
+}
+
+async function readData() {
+    console.info("reading data");
+
+    const dir = path.resolve("data", "csse_covid_19_data", "csse_covid_19_daily_reports");
+
+    const files = await readDir(dir);
+
+    return files
+        .filter(item => item.endsWith(".csv"))
+        .map(item => {
+
+            const dts = new Date(item.substr(0, item.length - 4)).toDateString();
+
+            return {
+                date: dts.substring(0, dts.length - 5),
+                path: path.resolve(dir, item)
+            }
+        });
+}
+
+async function compute(dayData: any[][]) {
 
     const values = dayData.reduce((vals, item, index) => {
 
@@ -107,7 +197,7 @@ async function exec() {
         values.drecovered.push(values.recovered[index] - values.recovered[index - 1]);
     }
 
-    await writeFile("./data.js", "data = " + JSON.stringify(values, null, 4));
+    return values;
 }
 
 
