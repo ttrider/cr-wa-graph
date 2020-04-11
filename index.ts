ced29be62f6e169994c79d96a29ad0967b7e01b7
TTRider <software@ttrider.com>
2020-04-11 13:51:03 -0700

    adding King county as a separate graph

--- index.ts
+++ index.ts
@@ -8,6 +8,21 @@ const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 const readDir = promisify(fs.readdir);
 
+interface DataValues {
+    confirmed: number[],
+    recovered: number[],
+    deaths: number[],
+    dconfirmed: number[],
+    drecovered: number[],
+    ddeaths: number[]
+}
+
+interface DataSet {
+    labels: string[],
+    main: DataValues,
+    focus: DataValues
+}
+
 exec().then(() => console.info("done"));
 
 async function exec() {
@@ -20,7 +35,8 @@ async function exec() {
     const nyValues = await compute(nyDayData);
 
     await writeFile("./data.js", "var wa_data = " + JSON.stringify(waValues, null, 4)
-    + "\nvar ny_data = " + JSON.stringify(nyValues, null, 4));
+        + "\nvar ny_data = " + JSON.stringify(nyValues, null, 4)
+    );
 }
 
 async function getWAData(dataSet: {
@@ -51,8 +67,13 @@ async function getWAData(dataSet: {
                 dp[1] += (item[3] ? parseInt(item[3]) : 0);
                 dp[2] += (item[4] ? parseInt(item[4]) : 0);
                 dp[3] += (item[5] ? parseInt(item[5]) : 0);
+
+                // dp[4] = dp[1];
+                // dp[5] = dp[2];
+                // dp[6] = dp[3];
+
                 return dp;
-            }, [item.date, 0, 0, 0] as any[]);
+            }, [item.date, 0, 0, 0, 0, 0, 0] as any[]);
 
             dayData.push(dataPoints);
 
@@ -61,7 +82,7 @@ async function getWAData(dataSet: {
             const lines = d
                 .filter((line, index) => {
                     if (line[3] != "US") return undefined;
-                    if (line[2] === "Washington") return line;
+                    if (line[2] == "Washington") return line;
                     return undefined;
                 });
 
@@ -70,8 +91,15 @@ async function getWAData(dataSet: {
                 dp[1] += parseInt(item[7]);
                 dp[2] += parseInt(item[8]);
                 dp[3] += parseInt(item[9]);
+
+                if (item[1] === "King") {
+                    dp[4] += parseInt(item[7]);
+                    dp[5] += parseInt(item[8]);
+                    dp[6] += parseInt(item[9]);
+                }
+
                 return dp;
-            }, [item.date, 0, 0, 0] as any[]);
+            }, [item.date, 0, 0, 0, 0, 0, 0] as any[]);
 
             dayData.push(dataPoints);
         }
@@ -110,7 +138,7 @@ async function getNYData(dataSet: {
                 dp[2] += (item[4] ? parseInt(item[4]) : 0);
                 dp[3] += (item[5] ? parseInt(item[5]) : 0);
                 return dp;
-            }, [item.date, 0, 0, 0] as any[]);
+            }, [item.date, 0, 0, 0, 0, 0, 0] as any[]);
 
             dayData.push(dataPoints);
 
@@ -144,7 +172,7 @@ async function getNYData(dataSet: {
                 dp[2] += parseInt(item[8]);
                 dp[3] += parseInt(item[9]);
                 return dp;
-            }, [item.date, 0, 0, 0] as any[]);
+            }, [item.date, 0, 0, 0, 0, 0, 0] as any[]);
 
             dayData.push(dataPoints);
         }
@@ -179,25 +207,54 @@ async function compute(dayData: any[][]) {
 
         if (item[1] > 1) {
             vals.labels.push(item[0]);
-            vals.confirmed.push(item[1]);
-            vals.deaths.push(item[2]);
-            vals.recovered.push(item[3]);
+
+            vals.main.confirmed.push(item[1]);
+            vals.main.deaths.push(item[2]);
+            vals.main.recovered.push(item[3]);
+
+            vals.focus.confirmed.push(item[4]);
+            vals.focus.deaths.push(item[5]);
+            vals.focus.recovered.push(item[6]);
         }
 
         return vals;
-    }, { labels: [], confirmed: [], recovered: [], deaths: [], dconfirmed: [], drecovered: [], ddeaths: [] } as { labels: string[], confirmed: number[], recovered: number[], deaths: number[], dconfirmed: number[], drecovered: number[], ddeaths: number[] });
+    }, {
+        labels: [],
+        main: {
+            confirmed: [],
+            recovered: [],
+            deaths: [],
+            dconfirmed: [],
+            drecovered: [],
+            ddeaths: []
+        },
+        focus: {
+            confirmed: [],
+            recovered: [],
+            deaths: [],
+            dconfirmed: [],
+            drecovered: [],
+            ddeaths: []
+        }
+    } as
+    DataSet);
 
-    values.dconfirmed.push(0);
-    values.ddeaths.push(0);
-    values.drecovered.push(0);
-
-    for (let index = 1; index < values.confirmed.length; index++) {
-        values.dconfirmed.push(values.confirmed[index] - values.confirmed[index - 1]);
-        values.ddeaths.push(values.deaths[index] - values.deaths[index - 1]);
-        values.drecovered.push(values.recovered[index] - values.recovered[index - 1]);
-    }
+    computeDiffs(values.main);
+    computeDiffs(values.focus);
 
     return values;
+
+    function computeDiffs(values: DataValues) {
+        values.dconfirmed.push(0);
+        values.ddeaths.push(0);
+        values.drecovered.push(0);
+
+        for (let index = 1; index < values.confirmed.length; index++) {
+            values.dconfirmed.push(values.confirmed[index] - values.confirmed[index - 1]);
+            values.ddeaths.push(values.deaths[index] - values.deaths[index - 1]);
+            values.drecovered.push(values.recovered[index] - values.recovered[index - 1]);
+        }
+    }
 }
 
 
